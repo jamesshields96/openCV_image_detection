d55cf00936409b07eeda788b84a68eb368dc490c
James Shields <jamesshields.96@gmail.com>
2022-05-05 21:05:36 +1000

    started base for generate_images.py

--- image_scripts/generate_images.py
+++ image_scripts/generate_images.py
@@ -1,9 +1,14 @@
 import capture_screen
 import pyautogui
+
 #TODO: Write script to cycle through FFXIV character creator and take screenshots
 def main():
-    print(pyautogui.size())
-    pass
+    screen = capture_screen.ScreenCapture()
+    screenWidth, screenHeight = pyautogui.size()
+    pyautogui.FAILSAFE = True
+    while True:
+        print(pyautogui.position())
+
 
 
 if __name__ == "__main__":
